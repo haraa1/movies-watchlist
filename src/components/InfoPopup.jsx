e4b2a13265d51df353c295f23a485eb09c64b6c2
haraa1 <hariscolakhodzic1@gmail.com>
2023-08-29 19:53:38 +0200

    [Button Behavior] Enable/Disable Functionality Based on Context

--- src/components/InfoPopup.jsx
+++ src/components/InfoPopup.jsx
@@ -7,8 +7,9 @@ const KEY = "4956f400";
 function InfoPopup({ selectedId, handleClearPopUp }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setLoading] = useState(false);
-  const { handleAddWatchlist } = useWatchlist();
+  const { watchlist, handleAddWatchlist } = useWatchlist();
 
+  const isOnWatchlist = watchlist.some((movie) => movie.imdbID === selectedId);
   const { Title: title } = movie;
   console.log(title);
 
@@ -50,17 +51,29 @@ function InfoPopup({ selectedId, handleClearPopUp }) {
           </div>
           <button
             onClick={() => handleAddWatchlist(movie)}
+            disabled={isOnWatchlist}
             className={styles.btnAdd}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 448 512"
-              fill="#ffffff"
-            >
-              <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-            </svg>
-            ADD TO WATCHLIST
+            {isOnWatchlist ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 448 512"
+                fill="#ffffff"
+              >
+                <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 448 512"
+                fill="#ffffff"
+              >
+                <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+              </svg>
+            )}
+            {isOnWatchlist ? "ADDED TO WATCHLIST" : "ADD TO WATCHLIST"}
           </button>
           )
         </>
