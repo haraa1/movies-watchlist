a41e64400d062491dc4f6a0ac60c7b62878794d4
haraa1 <hariscolakhodzic1@gmail.com>
2023-09-22 16:15:34 +0200

    Add Logout tab

--- src/components/PageSwap.jsx
+++ src/components/PageSwap.jsx
@@ -1,16 +1,22 @@
 import { Link, NavLink } from "react-router-dom";
 import styles from "./PageSwap.module.css";
+import Logout from "../features/authentication/Logout";
 
 function PageSwap() {
   return (
     <div className={styles.nav}>
       <ul>
         <li>
-          <NavLink to="/">All movies</NavLink>
+          <NavLink className={styles.activeLink} to="/home">
+            All movies
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/watchlist">Watchlist</NavLink>
+          <NavLink className={styles.activeLink} to="/watchlist">
+            Watchlist
+          </NavLink>
         </li>
+        <Logout />
       </ul>
     </div>
   );
