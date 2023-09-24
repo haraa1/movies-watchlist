456a4aae7b5a8ab1984e2a41c74c083707f2b09c
haraa1 <hariscolakhodzic1@gmail.com>
2023-09-25 00:06:16 +0200

    Update WatchlistBox

--- src/components/WatchlistBox.jsx
+++ src/components/WatchlistBox.jsx
@@ -1,15 +1,54 @@
+// import { useEffect, useState } from "react";
 import { useWatchlist } from "../contexts/WatchlistContext";
+// import { useUser } from "../features/authentication/useUser";
+// import supabase from "../services/supabase";
 import Movie from "./Movie";
 import styles from "./WatchlistBox.module.css";
 
 function WatchlistBox() {
   const { watchlist } = useWatchlist();
+  /* const { user } = useUser();
+  const [watchlist, setWatchlist] = useState([]);
+  useEffect(() => {
+    async function fetchWatchlist() {
+      const { data, error } = await supabase
+        .from("watchlist")
+        .select("movie_name, poster_path, movie_id")
+        .eq("user_id", user.id);
+      const renamedData = data.map((item) => ({
+        Title: item.movie_name,
+        Poster: item.poster_path,
+        MovieId: item.movie_id,
+      }));
+
+      if (error) {
+        console.error("Error fetching watchlist:", error.message);
+        return;
+      }
+
+      setWatchlist(renamedData);
+      console.log(watchlist);
+    }
+    fetchWatchlist();
+  }, [user]); */ //Selecting data from database
+
   return (
-    <ul className={styles.watchlist}>
-      {watchlist?.map((movie) => (
-        <Movie movie={movie} key={movie.imdbID} />
-      ))}
-    </ul>
+    <>
+      <h2>
+        {watchlist.length === 0
+          ? "Watchlist is currently empty"
+          : "Watchlist: "}
+      </h2>
+      <ul className={styles.watchlist}>
+        {watchlist?.map((movie, index) => (
+          <Movie
+            movie={movie}
+            key={movie.movie_id || index}
+            showStarAndDelete={true}
+          />
+        ))}
+      </ul>
+    </>
   );
 }
 
