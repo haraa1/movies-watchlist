6f546b7e1dd1d65232d161b9a40545ff5172c262
haraa1 <hariscolakhodzic1@gmail.com>
2023-09-24 22:36:15 +0200

    Add ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
 import { lazy } from "react";
-import { useState } from "react";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Watchlist from "./pages/Watchlist";
 import { WatchlistProvider } from "./contexts/WatchlistContext";
+import Login from "./pages/Login";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "./ProtectedRoute";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import AppLayout from "../AppLayout";
 
-const Homepage = lazy(() => import("./pages/HomePage"));
+const queryClient = new QueryClient({});
 
 export default function App() {
   return (
-    <WatchlistProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index path="/" element={<Homepage />} />
-          <Route path="/watchlist" element={<Watchlist />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </WatchlistProvider>
+    <QueryClientProvider client={queryClient}>
+      <WatchlistProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to="/home" />} />
+              <Route path="/home" index element={<HomePage />} />
+              <Route path="/watchlist" element={<Watchlist />}></Route>
+            </Route>
+
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </BrowserRouter>
+        <Toaster toastOptions={{ style: { fontSize: "14px" } }} />
+      </WatchlistProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
